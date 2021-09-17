91b456ac1306caf0b9090b880a9df9c6de16a176
nchitha <chithambaram.narendrakumar.tpr@in.pwc.com>
2021-09-17 16:58:01 +0530

    adding provider

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { AuthModule } from './auth/auth.module';
 import { AppMaterialModule } from './app-material/app-material.module';
+import { AuthService } from './_services/auth.service';
 @NgModule({
   declarations: [
     AppComponent
@@ -14,13 +14,11 @@ import { AppMaterialModule } from './app-material/app-material.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule,
-    ReactiveFormsModule,
     HttpClientModule,
     AppMaterialModule,
     AuthModule
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
